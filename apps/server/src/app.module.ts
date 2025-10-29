b578b321572f3bba51010d6da5279034daae8f05
Polash <34005640+sshahriazz@users.noreply.github.com>
2025-10-29 15:20:20 +0600

    feat: add core infrastructure and security features

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,6 +1,52 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR, APP_GUARD } from '@nestjs/core';
+import { CacheInterceptor } from '@nestjs/cache-manager';
+import configuration from './config/configuration';
+import { HttpModule } from '@nestjs/axios';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local', '.env.production'],
+      load: [configuration],
+      cache: true,
+      isGlobal: true,
+    }),
+    CacheModule.register({
+      ttl: 60 * 60 * 1000,
+      max: 100,
+      isGlobal: true,
+      store: 'memory',
+    }),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+    TerminusModule
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+  controllers: [HealthController],
 })
 export class AppModule {}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,10 +1,57 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
+import compression from 'compression';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Configuration,  CorsConfiguration, OpenapiConfiguration } from './config/configuration';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'fatal'],
+  });
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port')!;
+  const corsConfig = configService.get<CorsConfiguration>('cors')!;
+  const openapiConfig = configService.get<OpenapiConfiguration>('openapi')!;
+
+  app.use(compression());
+  app.use(helmet());
+  app.enableCors({
+    origin: corsConfig.origins,
+    credentials: corsConfig.credentials,
+    methods: corsConfig.methods,
+    allowedHeaders: corsConfig.allowedHeaders,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      skipMissingProperties: false,
+      skipUndefinedProperties: true,
+      stopAtFirstError: false,
+    }),
+  );
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle(openapiConfig.title)
+    .setDescription(openapiConfig.description)
+    .setVersion(openapiConfig.version)
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // app.setGlobalPrefix('api');
+  await app.listen(port);
+  Logger.debug(`Server started on http://localhost:${port}`);
+  Logger.debug(`OpenAPI documentation available at http://localhost:${port}/api`);
 }
 bootstrap()
-  .then(() => console.log('Server started on port 4000'))
   .catch((err) => console.error(err));
